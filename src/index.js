99831651a6c28ebf58fa7aef7d5b15bd02cd768e
anis <achadlia0@gmail.com>
2023-05-07 15:52:10 +0300

    a card with react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import Style from "./style.css"
+import Profil from "./icons/profil.png"
+import GitHubIcon from "./icons/GitHub Icon.png"
+import LinkedinIcon from "./icons/linkedin.png"
+import MailIcon from "./icons/mail.png"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+    return (
+        <section className="app">
+            <figure>
+                <img src={Profil} alt="Anis" className="profil"></img>
+            </figure>
+            <div className="main">
+                <div className="title">
+                    <h1>Anis Shadlia</h1>
+                    <h2>Frontend Developer</h2>
+                    <a href="https://shadlia.com" target="_blank" rel="noreferrer" className="website">shadlia.com</a>
+                    <a href="mailto:info@shadlia.com" target="_blank" rel="noreferrer" className="mail"><img src={MailIcon} alt="mail-icon"></img> <span>Email</span></a>
+                </div>
+                <div className="descreption">
+                    <div>
+                        <h3>About</h3>
+                        <p>
+                            I am a frontend developer with a particular interest in making
+                            things simple and automating daily tasks. I try to keep up with 
+                            security and best practices, and am always looking for new things to learn.
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Interests</h3>
+                        <p>
+                        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. 
+                        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="footer">
+                <a href="https://github.com/Anis-Web" target="_blank" rel="noreferrer"><img src={GitHubIcon} alt="github-icon"></img></a>
+                <a href="https://github.com/Anis-Web" target="_blank" rel="noreferrer"><img src={LinkedinIcon} alt="linkedin-icon"></img></a>
+                <a href="https://github.com/Anis-Web" target="_blank" rel="noreferrer"><img src={MailIcon} alt="mail-icon"></img></a>
+            </div>
+        </section>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App/>, document.getElementById("root"))
